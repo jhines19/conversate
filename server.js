a69f88be9add0c87160e73f40bf026fec5919dbe
Jaraad Hines <jaraadhines@jaraads-mbp-2.myfiosgateway.com>
2021-11-19 07:17:22 -0500

    run server code

--- server.js
+++ server.js
@@ -3,9 +3,8 @@ const express = require('express');
 const session = require('express-session');
 const exphbs = require('express-handlebars');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
-const routes = require('./controllers');
 const sequelize = require('./config/connection');
-
+const routes = require('./controllers');
 const helpers = require('./utils/helpers');
 const hbs = exphbs.create({ helpers });
 
